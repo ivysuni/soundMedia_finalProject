a1517455b00088bcb106bedbec2462ef2cbe3d77
은결 <142661679+ivysuni@users.noreply.github.com>
2024-12-14 00:34:43 +0900

    v02

--- sketch.js
+++ sketch.js
@@ -1,25 +1,48 @@
-let noOfStars = 40, sizeDiff = 10, majorAxisMinLen = 10, widthHeightRatio = 0.7, rotationGradient, rotationGradientSlider;
+let noOfStars = 4000, sizeDiff = 0.1, majorAxisMinLen = 10, widthHeightRatio = 0.7, rotationGradient, rotationGradientSlider, stars = [];
 
 function setup() {
   createCanvas(640, 640);
   rotationGradient = PI/noOfStars;
-  rotationGradientSlider = createSlider(-0.2, 0.2, rotationGradient, 0.01)
+  rotationGradientSlider = createSlider(0,rotationGradient*5, rotationGradient, rotationGradient/100);
+  for(let i=0;i<noOfStars;i++) {
+    const majorAxisLen = majorAxisMinLen + i*sizeDiff;
+    stars.push(new Star(majorAxisLen));
+  }
 }
 
 function draw() {
-  background(0);
+  background('black');
   
   noFill();
-  stroke(255);
+  stroke('white');
+
+  translate(width/2, height/2)
   
-  translate(width/2, height/2);
+  for(let i=0;i<noOfStars;i++) {    
+    rotate(rotationGradientSlider.value())
+    stars[i].display();
+    stars[i].update();
+  }
+}
+
+class Star {
+  constructor(majorAxisLen) {
+    this.majorAxisLen = majorAxisLen;
+    this.minorAxisLen = majorAxisLen * widthHeightRatio;
+    this.theta = random(2*PI);
+    this.deltaTheta = 0.01
+  }
   
-  for(let i=0; i<noOfStars; i++) {
-    const majorAxisLen = majorAxisMinLen + i*sizeDiff;
-    const minorAxisLen = majorAxisLen * widthHeightRatio;
+  display() {
+    const x = (this.majorAxisLen/2)*cos(this.theta)
+    const y = (this.minorAxisLen/2)*sin(this.theta)
     
-    rotate(rotationGradientSlider.value());
-    
-    ellipse(0, 0, majorAxisLen, minorAxisLen);
+    noStroke();
+    fill(255,255,255,100);
+    circle(x, y, 2);
+  }
+  
+  update() {
+    this.theta += this.deltaTheta;
   }
 }
